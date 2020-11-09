7e8dec30ad51f44a3b841e1084c7750182d82567
Grace Fagan <gfagan01@tufts.edu>
2020-11-09 00:24:27 -0500

    heatmap labels

--- public/js/vis.js
+++ public/js/vis.js
@@ -985,9 +985,9 @@ function initFailedAttemptsMap(data) {
 
   console.log(data[1])
 
-  var margin = {top: 50, right:10, bottom: 50, left: 30},
+  var margin = {top: 0, right:10, bottom: 50, left: 30},
     width = 400 - margin.left - margin.right,
-    height = 115 - margin.top - margin.bottom;
+    height = 70 - margin.top - margin.bottom;
 
   d3.select("#failed_attemptsMAP").selectAll("*").remove();
 
@@ -1005,7 +1005,7 @@ function initFailedAttemptsMap(data) {
   // var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
   var x = d3.scaleBand()
-    .range([ 0, width ])
+    .range([ 35, width ])
     .domain(Object.keys(puzzle_categories))
     .padding(0.01);
     
@@ -1071,6 +1071,13 @@ function initFailedAttemptsMap(data) {
   .on("mousemove", mousemove)
   .on("mouseleave", mouseleave);
 
+  failed_attempts_map.svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", 25)
+    .attr("y", height)
+    .text("Fails");
+
   var ReAttemptMap = failed_attempts_map.svg.selectAll()
   .data(data[1])
   .enter().append("rect")
@@ -1086,6 +1093,13 @@ function initFailedAttemptsMap(data) {
   .on("mouseover", mouseover)
   .on("mousemove", mousemove)
   .on("mouseleave", mouseleave);
+
+  failed_attempts_map.svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", 30)
+    .attr("y", height + 25)
+    .text("Reattempts");
 
   return failed_attempts_map;
 }
